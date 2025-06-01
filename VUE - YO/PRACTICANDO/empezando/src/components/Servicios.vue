8a5f1ae516741eaa5c294b1f129c3667b460ae26
José Ivan Mesia Portocarrero <jmesia.portocarrero@gmail.com>
2025-06-01 17:45:59 +0200

    01/06/2025 - 17:45

---
+++ VUE - YO/PRACTICANDO/empezando/src/components/Servicios.vue
@@ -0,0 +1,33 @@
+<template>
+
+
+  <div class="shadows mt-5 container row p-0">
+    <h1 class="mt-4">Ejercicio 4: Componentes</h1>
+    <h1 class="mt-4">Nuestros Servicios</h1>
+    <!-- <div class="row"> -->
+      <div v-for="(serv, index) in servicios" :key='index' class="card border shadow mt-5 col-3 p-2 me-0 text-center">
+        <img :src="serv.imagen_url" alt="Imagen del servicio" class="" />
+        <div class="card-body">
+          <h5 class="card-title">{{ serv.titulo }}</h5>
+          <p v-if="!serv.descripcion || serv.descripcion.length === 0" class="card-text">Sin descripción</p>
+          <p v-else>{{ serv.descripcion }}</p>
+          <button v-if="'texto_boton' in serv" class="btn btn-primary">
+            {{ serv.texto_boton }} 
+          </button>
+          <button v-else class="btn btn-primary">Contratar</button>
+        </div>
+      </div>
+    </div>
+  <!-- </div> -->
+
+
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { servicios } from '../servicios'
+
+</script>
+
+<!-- <style>
+</style> -->
---
+++ VUE - YO/PRACTICANDO/empezando/src/components/Tareas.vue
@@ -0,0 +1,50 @@
+<template>
+    <!-- EJERCICIO 3 -->
+
+    <div class="mt-5 pt-5">
+        <h2 class="pb-5"> José Ivan - Ejercicio 3: Lista de tareas</h2>
+        <ul class="list-unstyled">
+            <li v-for="(tarea, index) in listaTareas" :key="index" class="row p-3 border">
+                <h4 v-if="tarea.completada" class="m-0 p-1 col-10 text-decoration-line-through">{{ tarea.texto }}</h4>
+                <h4 v-else class="m-0 p-1 col-10">{{ tarea.texto }}</h4>
+                <button v-if="tarea.completada" class="col-2 btn btn-danger" @click="cambiarEstadoTarea(index)">{{
+                    tarea.completada ? 'Finalizada' : 'Pendiente' }}</button>
+                <button v-else class="col-2 btn btn-success" @click="cambiarEstadoTarea(index)">{{ tarea.completada ?
+                    'Finalizada' : 'Pendiente' }}</button>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+
+import { ref } from 'vue'
+import { listaTareas } from '../tareas'
+
+
+
+function cambiarEstadoTarea(index) {
+    listaTareas.value[index].completada = !listaTareas.value[index].completada
+}
+
+
+
+</script>
+
+<!-- <style scoped>
+.principal {
+    background-color: #ffffff;
+    padding: 20px;
+    /* border: 1px solid #ccc; */
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0);
+    height: 100vh;
+    margin-left: 50px;
+    margin-right: 50px;
+}
+.secundario {
+    text-align: center;
+    border-radius: 10px;
+    padding: 20px;
+}
+</style> -->
